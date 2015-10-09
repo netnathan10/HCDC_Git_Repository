ba9076a11ccfe814d42a46e5750de13c0a5e9d59
nathanberg <nathan.berg@verodin.com>
2015-10-08 19:57:13 -0700

    initial commit

---
+++ nmap_script/nmap_script.py
@@ -0,0 +1,27 @@
+import nmap
+from optparse import OptionParser
+import time
+
+parser = OptionParser()
+parser.add_option("-s", "--hosts", dest="hosts")
+parser.add_option("-p", "--ports", dest="ports")
+# parser.add_option("-i", "--interval", type="int", dest="num")
+
+(options, args) = parser.parse_args()
+
+while True:
+    nm = nmap.PortScanner()
+    nm.scan(options.hosts, options.ports)
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
+
+            lport = nm[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    time.sleep(60)
