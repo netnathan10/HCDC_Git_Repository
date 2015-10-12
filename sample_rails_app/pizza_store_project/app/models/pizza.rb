935315c4a3f5888a09e34b27df4fcb90ed7a3304
nathanberg <nathan.berg@verodin.com>
2015-10-11 17:14:41 -0700

    Pizza model validations added

--- sample_rails_app/pizza_store_project/app/models/pizza.rb
+++ sample_rails_app/pizza_store_project/app/models/pizza.rb
@@ -1,2 +1,15 @@
 class Pizza < ActiveRecord::Base
+
+  def legit_toppings_yo
+    ['cheese', 'olives', 'sausage', 'pepperoni', 'anchovies']
+  end
+
+  validates_presence_of :size, :allow_blank => false
+  validates_presence_of :delivery, :allow_blank => false
+  validates_presence_of :sauce, :allow_blank => false
+  validates_presence_of :crust, :allow_blank => false
+  validates :toppings, :inclusion => {:in => legit_toppings_yo, :message => 'invalid topping selected' }
+  validates :size, :inclusion => {:in => ['small', 'medium', 'large', 'extra_large', 'wumbo'], :message => 'invalid size selected' }
+  validates :crust, :inclusion => {:in => ['regular', 'thin'], :message => 'invalid crust selected' }
+
 end
