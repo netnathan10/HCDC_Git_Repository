12f7e5b5faa23938e12f0b104d88d6ae286fed68
nathanberg <nathan.berg@verodin.com>
2015-10-11 17:41:52 -0700

    Add ability for controller to create an order in the db

--- sample_rails_app/pizza_store_project/app/controllers/pizza_controller.rb
+++ sample_rails_app/pizza_store_project/app/controllers/pizza_controller.rb
@@ -1,2 +1,14 @@
 class PizzaController < ApplicationController
+
+  def create_order
+    @pizza = Pizza.new(params[:pizza])
+    @toppings_array = @pizza.toppings.split(',')
+    @pizza.price = @pizza.calculate_price
+
+    if @pizza.save
+      # Go to order-review form (doesn't exist yet)
+    else
+      # Throw an error
+    end
+  end
 end
