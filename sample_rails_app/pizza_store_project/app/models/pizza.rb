581d2c5c120126032a6edc7db45e36244ffa3099
nathanberg <nathan.berg@verodin.com>
2015-10-11 17:41:19 -0700

    Add ability to calculate price

--- sample_rails_app/pizza_store_project/app/models/pizza.rb
+++ sample_rails_app/pizza_store_project/app/models/pizza.rb
@@ -4,12 +4,36 @@ class Pizza < ActiveRecord::Base
     ['cheese', 'olives', 'sausage', 'pepperoni', 'anchovies']
   end
 
+  def sizes
+    {
+      'small' => {1.0},
+      'medium' => {2.0},
+      'large' => {3.0},
+      'extra_large' => {4.0},
+      'wumbo' => {5.0}
+    }
+  end
+
+  def crust
+    {
+      'regular' => {1.0},
+      'thin' => {1.50}
+    }
+  end
+
   validates_presence_of :size, :allow_blank => false
   validates_presence_of :delivery, :allow_blank => false
   validates_presence_of :sauce, :allow_blank => false
   validates_presence_of :crust, :allow_blank => false
   validates :toppings, :inclusion => {:in => legit_toppings_yo, :message => 'invalid topping selected' }
-  validates :size, :inclusion => {:in => ['small', 'medium', 'large', 'extra_large', 'wumbo'], :message => 'invalid size selected' }
-  validates :crust, :inclusion => {:in => ['regular', 'thin'], :message => 'invalid crust selected' }
+  validates :size, :inclusion => {:in => sizes.keys, :message => 'invalid size selected' }
+  validates :crust, :inclusion => {:in => crust.keys, :message => 'invalid crust selected' }
 
+  def calculate_price
+    price = 4.99
+    price += (toppings.size * .50)
+    price += sizes['size']
+    price += crust['crust']
+    return price
+  end
 end
