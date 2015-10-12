dca20df512045f4449952c7f57708f5674e6d196
nathanberg <nathan.berg@verodin.com>
2015-10-11 18:26:44 -0700

    Fixing up broken stuff in model

--- sample_rails_app/pizza_store_project/db/schema.rb
+++ sample_rails_app/pizza_store_project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012000335) do
+ActiveRecord::Schema.define(version: 20151012003129) do
 
   create_table "pizzas", force: :cascade do |t|
     t.string   "toppings"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151012000335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "delivery"
+    t.float    "price"
   end
 
   create_table "users", force: :cascade do |t|
